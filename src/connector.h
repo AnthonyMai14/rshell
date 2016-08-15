3afaa989c8c99efdc04e30e8357840f8d8f97235
Anthony Mai <amai005@well.cs.ucr.edu>
2016-08-15 02:05:09 -0700

    Add constructor & subclasss of connector in connector.h

--- src/connector.h
+++ src/connector.h
@@ -1,7 +1,45 @@
 #ifndef CONNECTOR_H
 #define CONNECTOR_H
 
+#include <string>
+
 public Connector {
-	private:
-	public:
+    protected:
+        string connector_id;
+    public:
+        //constructor
+        Connector();
+        
+        //virtual function
+        virtual void runRshell() = 0;
 }
+
+public Add : public Connector {
+    public:
+        //constructor
+        Add() : connector_id("&&") {};
+	
+	void runRshell() {
+	
+	};
+};
+
+public Or : public Connector {
+    public:
+	//constructor
+	Or() : connector_id("||") {};
+
+	void runRshell() {
+	
+	};
+};
+
+public SemiColon : public Connector {
+    public:
+	//constructor
+	SemiColon : connector_id(";") {};
+	
+	void runRshell() {
+	
+	};
+};
