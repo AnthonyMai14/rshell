655890949d71c50b16c94108cd06872ad9725f35
anthonymai14 <amai005@ucr.edu>
2016-08-16 20:24:41 +0000

    Made class declaration changes to fix error

--- src/connector.h
+++ src/connector.h
@@ -2,14 +2,15 @@
 #define CONNECTOR_H
 
 #include "terminal.h" //include strings
-
+#include <iostream>
 class Connector : public Terminal{
     //inherited attribute: std::string terminal_type
     protected:
         std::string connector_type;
     public:
         //constructor
-        Connector() : Terminal("conn") {};
+        Connector() : Terminal("conn") {
+        };
         
         //virtual function
         void runRshell();
@@ -23,7 +24,9 @@ class Connector : public Terminal{
 class Add : public Connector {
     public:
         //constructor
-        Add() : Connector(), connector_type("&") {};
+        Add() {
+            connector_type = "&";
+        };
 	
 	void runRshell() {
 	
@@ -33,7 +36,9 @@ class Add : public Connector {
 class Or : public Connector {
     public:
 	//constructor
-	Or() : connector_type("|") {};
+	Or()  {
+	    connector_type = "|";
+	};
 
 	void runRshell() {
 	
@@ -43,7 +48,9 @@ class Or : public Connector {
 class SemiColon : public Connector {
     public:
 	//constructor
-	SemiColon :  connector_type(";") {};
+	SemiColon() {
+	    connector_type = ";";
+	};
 	
 	void runRshell() {
 	
