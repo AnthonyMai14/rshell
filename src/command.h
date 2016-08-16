60ccb228d1fb885ea16e6180f626c11cf272793c
anthonymai14 <amai005@ucr.edu>
2016-08-16 12:42:03 +0000

    Made changes to command.h

--- src/command.h
+++ src/command.h
@@ -1,8 +1,15 @@
 #ifndef COMMAND_H
 #define COMMAND_H
 
+#include <string>
+
 class Command {
 	private:
+		std::string cmd;
 	public:
+		Command() : cmd("") {};
+		void setCmd(std::string cmd) {
+			this->cmd = cmd;
+		}
 };
 #endif
