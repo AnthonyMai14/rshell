6a8874c4f61db0a01d6df5381de46525c2e161cf
anthonymai14 <amai005@ucr.edu>
2016-08-16 12:43:59 +0000

    Made instantiate changes to rshell

--- src/rshell.h
+++ src/rshell.h
@@ -1,7 +1,7 @@
 #ifndef RSHELL_H
 #define RSHELL_H
 
-//#include "command.h"
+#include "command.h"
 //#include "connector.h"
 
 #include <iostream>
@@ -13,34 +13,40 @@
 class RShell {
     protected:
 		std::string input;
-		//std::vector<Command*> v;
+		std::vector<Command*> v;
     public:
 		RShell(){
 		    input = "";	
 		};
-	
-	void run() {
-	    bool user_continue = true;
-	    
-	    while (user_continue) {
-			std::cout << '$';
-			std::string user_input = "";
-			getline(std::cin, user_input);
-			
-			//if exit, terminate program
-			if (user_input == "exit" || user_input == "EXIT") {
-				exit(EXIT_SUCCESS);
-			}
-			
-			//take out comments
-			for (unsigned i = 0; i < user_input.size(); ++i) {
-			    if (user_input.at(i) == '#') {
-					user_input = user_input.substr(0, i);
-			    }
-			}
+		
+		void run();
+	// void parse(std::string user_input, std::vector<Command*> &v) {
+	// 	for (int i = 0; i < user_input; ++i) {
+	// 	}	
+	// };
+};
 
-			//iterate through user_input & parse through it
-	    }
-	};
+void RShell::run() {
+    bool user_continue = true;
+    
+    while (user_continue) {
+		std::cout << '$';
+		std::string user_input = "";
+		getline(std::cin, user_input);
+		
+		//if exit, terminate program
+		if (user_input == "exit" || user_input == "EXIT") {
+			exit(EXIT_SUCCESS);
+		}
+		
+		//take out comments
+		for (unsigned i = 0; i < user_input.size(); ++i) {
+		    if (user_input.at(i) == '#') {
+				user_input = user_input.substr(0, i);
+		    }
+		}
+
+		//iterate through user_input & parse through it
+    }
 };
 #endif
