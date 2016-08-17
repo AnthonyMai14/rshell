857a34d7cb11680c333718064b86dbfa80d2261b
Saburo Nakano <snaka010@well.cs.ucr.edu>
2016-08-16 23:08:21 -0700

    Added an additional three command case

--- test/multi_command.sh
+++ test/multi_command.sh
@@ -1,7 +1,8 @@
 echo hello && ls -a
 ls -a && echo "cS 100 is the best"
 echo hello || echo yooooo
-ls -a && mkdir test || echo "Thank you" ; echo "time to cry"
-ls -q || ls -a
+ls -a && ls -wasd || echo "Thank you" ; echo "time to cry"
+ls -qwop || ls -a
 echo yep ;  echo "we are done"
+ls -1 && ls -a ; ls -dsac || echo "did it work?"
 
