dea5d6adf79e066dfaaba7ab56069f3d29fa3950
Saburo Nakano <snaka010@well.cs.ucr.edu>
2016-08-16 22:37:33 -0700

    cretead multi command test cases

---
+++ test/multi_command.sh
@@ -0,0 +1,7 @@
+echo hello && ls -a
+ls -a && echo "cS 100 is the best"
+echo hello || echo yooooo
+ls -a && mkdir test || echo "Thank you" ; echo "time to cry"
+ls -q || ls -a
+echo yep ;  echo "we are done"
+
