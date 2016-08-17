ff634adac50eec1c3b3ba0c96e1bff179c63dff8
Saburo Nakano <snaka010@well.cs.ucr.edu>
2016-08-16 22:36:58 -0700

    created a comment command test

---
+++ test/commented_command.sh
@@ -0,0 +1,6 @@
+echo Hello #prints out hello ?
+ls -a #checking if this will work 
+echo hiya ; echo world #checking for connectors 
+ls -a #&& the connector should not be activated
+ls -x || echo sucess #|| connector should work
+
